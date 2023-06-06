3c4fb7dfa02ad204fbf2ac16d34008064adf0db5
sivadeepkumar <sivadeepkumar3@gmail.com>
2023-06-06 08:14:43 +0000

    ReactJs_Prime_nonPrime_ThreedotsLoading

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import AllProductsSection from '../AllProductsSection'
+
+import Header from '../Header'
+import PrimeDealsSection from '../PrimeDealsSection'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <PrimeDealsSection />
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
